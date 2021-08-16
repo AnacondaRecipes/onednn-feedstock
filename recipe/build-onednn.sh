60d9a13509dcf7ad45ca3903f9eedab13c3121e8
Anthony DiPietro <adipietro@anaconda.com>
2021-08-16 13:47:57 -0400

    Recipe updates for Intel variants on linux-64

--- recipe/build-onednn.sh
+++ recipe/build-onednn.sh
@@ -14,17 +14,32 @@ fi
 mkdir build-${dnnl_cpu_runtime}
 pushd build-${dnnl_cpu_runtime}
 
+# default to off
+DNNL_GPU_RUNTIME="NONE"
+
 if [[ "${dnnl_cpu_runtime}" == "tbb" ]]; then
   export TBBROOT=${PREFIX}
   DNNL_CPU_RUNTIME="TBB"
 elif [[ "${dnnl_cpu_runtime}" == "omp" ]]; then
   DNNL_CPU_RUNTIME="OMP"
+elif [[ "${dnnl_cpu_runtime}" == "iomp" ]]; then
+  DNNL_CPU_RUNTIME="OMP"
+elif [[ "${dnnl_cpu_runtime}" == "dpcpp" ]]; then
+  # see this guide for oneAPI DPC++ Compiler: https://oneapi-src.github.io/oneDNN/dev_guide_build.html
+  export CC=${HOST}-clang
+  export CXX=${HOST}-clang++
+  export TBBROOT=${PREFIX}
+  # need to point to OpenCL?
+  CMAKE_ARGS="${CMAKE_ARGS} -DOpenCL_INCLUDE_DIR=$(${CC} -print-resource-dir)/include"
+  DNNL_CPU_RUNTIME="DPCPP"
+  DNNL_GPU_RUNTIME="DPCPP"
 elif [[ "${dnnl_cpu_runtime}" == "threadpool" ]]; then
   DNNL_CPU_RUNTIME="THREADPOOL"
 fi
+
 cmake ${CMAKE_ARGS} -GNinja \
   -DDNNL_CPU_RUNTIME=${DNNL_CPU_RUNTIME} \
-  -DDNNL_GPU_RUNTIME=NONE \
+  -DDNNL_GPU_RUNTIME=${DNNL_GPU_RUNTIME} \
   ..
 ninja install
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != 1 ]]; then
